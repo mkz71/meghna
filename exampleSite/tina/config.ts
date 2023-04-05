4fd811d1884addc376a58c5377b1150b7ac08f6a
mkz71 <m.horburapa@gmail.com>
2023-04-05 19:42:08 -0400

    Add TinaCMS

---
+++ exampleSite/tina/config.ts
@@ -0,0 +1,157 @@
+import { defineConfig } from "tinacms";
+import { authorFields } from "./templates";
+import { postFields } from "./templates";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: null, // Get this from tina.io
+  token: null, // Get this from tina.io
+  client: { skip: true },
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "static",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        format: "md",
+        label: "Post (en)",
+        name: "post__en_",
+        path: "exampleSite/content/english/blog",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+          ...postFields(),
+        ],
+      },
+      {
+        format: "md",
+        label: "Post (fr)",
+        name: "post__fr_",
+        path: "exampleSite/content/french/blog",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+          ...postFields(),
+        ],
+      },
+      {
+        format: "md",
+        label: "Author (en)",
+        name: "author__en_",
+        path: "exampleSite/content/english/author",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+          ...authorFields(),
+        ],
+      },
+      {
+        format: "md",
+        label: "Author (fr)",
+        name: "author__fr_",
+        path: "exampleSite/content/french/author",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+          ...authorFields(),
+        ],
+      },
+      {
+        format: "md",
+        label: "Homepage",
+        name: "homepage",
+        path: "exampleSite/data",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        format: "md",
+        label: "i18n",
+        name: "i18n",
+        path: "exampleSite/i18n",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        format: "toml",
+        label: "Configuration",
+        name: "configuration",
+        path: "exampleSite",
+        match: {
+          include: "*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
